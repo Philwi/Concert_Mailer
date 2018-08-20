77b4b43f8db9f1b39980cce4c46d2d4f094d2982
Philwi <philoc@freenet.de>
2018-08-20 13:40:44 +0200

    smtp für emails in production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,7 +19,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'confirmation@concert_mailer.com'
+  config.mailer_sender = 'philoc@freenet.de'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
