1d034e99ff0e249553f56cce82bf3dd89eec8094
Philwi <philoc@freenet.de>
2018-08-21 08:19:41 +0200

    landingpage redirect + seeds

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,12 +1,17 @@
 class LandingPageController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :go_to_dashboard
+  before_action :go_to_not_activated
   layout 'landing_page'
   def index
     @user = User.new
   end
 
   def not_active
+
+  end
+
+  def not_confirmed
 
   end
 
@@ -19,8 +24,17 @@ class LandingPageController < ApplicationController
     if current_user && current_user.active
       redirect_to(dashboard_path)
     end
+  end
+
+  def go_to_not_activated
     if current_user && !current_user.active && request.fullpath != ("/not_active")
        redirect_to "/not_active"
+    end
+  end
+
+  def go_to_not_confirmed
+    if current_user && !current_user.active && current_user.confirmed_at.nil? && request.fullpath != ("/not_confirmed")
+       redirect_to "/not_confirmed"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   get "bands/which_edit", to: "bands#which_edit", as: :bands_which_edit
   get "calendar", to: "events#calendar", as: :calendar
   get "not_active", to: "landing_page#not_active"
+  get "not_confirmed", to: "landing_page#not_confirmed"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '*path', to: redirect('/'), via: :all
 end
